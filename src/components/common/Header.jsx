83c41dc1b00d5352b60f9801e5a2d92345e173db
RishabGiakaa2245 <rishab@giakaa.com>
2025-07-31 14:08:13 +0530

    css issue

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,7 +2,6 @@
 import React, { useState } from 'react';
 import Button from '../ui/Button';
 import { useIntersectionObserver } from '@/hooks/useIntersectionObserver';
-import '@/styles/animations.css';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
